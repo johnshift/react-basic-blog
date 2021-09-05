2dc2508eff70cc0103db520bb4c1273edfca989f
John Ballesteros <johnshift.dev@gmail.com>
2021-09-05 13:13:06 +0800

    add error handling

--- src/Home.js
+++ src/Home.js
@@ -3,25 +3,38 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     // mimic 1 sec response time
     setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
+      fetch("http://localhost:8000/blogss")
         .then((res) => {
+          // handle server errors
+          if (!res.ok) {
+            throw Error("could not fetch the data for that resource");
+          }
+
           return res.json();
         })
         .then((data) => {
           setBlogs(data);
           setIsPending(false);
+
+          setError(null);
+        })
+        // server error: e.g. server unreachable
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
         });
     }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>error: {error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
