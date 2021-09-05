f32a7fdcc1e8361399be1b695b308c617d8a4705
John Ballesteros <johnshift.dev@gmail.com>
2021-09-05 15:39:07 +0800

    add fetch abort, to stop fetch request when on different page

--- src/useFetch.js
+++ src/useFetch.js
@@ -8,9 +8,11 @@ const useFetch = (url) => {
   // make sure to include [url] as dependency
   // to ensure that this function runs whenever url changes
   useEffect(() => {
+    const abortCtl = new AbortController();
+
     // mimic 1 sec response time
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCtl.signal })
         .then((res) => {
           // handle server errors
           if (!res.ok) {
@@ -27,10 +29,17 @@ const useFetch = (url) => {
         })
         // server error: e.g. server unreachable
         .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            setIsPending(false);
+            setError(err.message);
+          }
         });
     }, 1000);
+
+    // cleanup fetch
+    return () => abortCtl.abort();
   }, [url]);
 
   return { data, isPending, error };
